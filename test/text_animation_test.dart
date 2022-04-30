2ce8cfe1117bbab319c1feb1b6008d8602401e8f
Farhan Mac <farhan.fadila1717@gmail.com>
2022-04-30 09:29:01 +0700

    Add text animation test

---
+++ test/text_animation_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:slide_countdown/slide_countdown.dart';
+import 'package:slide_countdown/src/utils/text_animation.dart';
+
+const kDuration = Duration(minutes: 3);
+
+void main() {
+  late ValueNotifier<int> notifier;
+
+  setUpAll(() {
+    notifier = ValueNotifier(0);
+  });
+
+  testWidgets('Text animation test', (tester) async {
+    final widget = TextAnimation(
+      value: notifier,
+      textStyle: TextStyle(),
+      slideDirection: SlideDirection.up,
+      slideAnimationDuration: const Duration(milliseconds: 200),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: widget,
+      ),
+    );
+
+    await tester.pumpAndSettle();
+
+    final text1 = find.text("0");
+
+    expect(text1, findsWidgets);
+
+    notifier.value = 1;
+
+    await tester.pumpAndSettle();
+
+    final text2 = find.text("1");
+
+    expect(text2, findsWidgets);
+  });
+}
