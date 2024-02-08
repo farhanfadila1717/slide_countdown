09647e06acd6dea2aedb16f08b770f5621e6f8b1
Farhan <farhan.fadila@digits.com>
2024-02-08 10:45:14 +0700

    refactor: stream duration

---
+++ lib/src/data/duration_listener.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+
+class DurationListener extends ValueNotifier<Duration> {
+  DurationListener([super.value = Duration.zero]);
+}
